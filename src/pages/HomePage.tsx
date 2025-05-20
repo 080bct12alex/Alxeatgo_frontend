a12e7ad5b0311266298407163b97c96945ba7690
Alex Shrestha <080bct012.alex@pcampus.edu.np>
2025-05-20 23:02:46 +0545

    first commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,47 @@
+import landingImage from "../assets/landing.png";
+import appDownloadImage from "../assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
+
+  return (
+    <div className="flex flex-col gap-12">
+      <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+        <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+           Craving Something Delicious?
+        </h1>
+        <span className="text-xl">Find your favorite meals in  just a click !</span>
+        <SearchBar
+          placeHolder="Search by City or Town"
+          onSubmit={handleSearchSubmit}
+        />
+      </div>
+        
+        
+      {/* no mobile app yet, only for better ui */}
+      <div className="grid md:grid-cols-2 gap-5">
+        <img src={landingImage} />
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
+          <span className="font-bold text-3xl tracking-tighter">
+             Get food faster, your way!
+          </span>
+          <span>
+            Download the Alxeatgo App for faster ordering and personalised
+            recommendations
+          </span>
+          <img src={appDownloadImage} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
